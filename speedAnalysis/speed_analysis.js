5d7b99a4b6bbee5414df6390ba4b39d5022f467c
victorgrigoras66-del <victorgrigoras66@gmail.com>
2026-02-20 03:18:58 -0500

    message

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -41,6 +41,7 @@ startTime = new Date().getTime();
             // Display the results
             var outputDiv = document.getElementById("output");
             outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+                "<p>Total Lenght: " + userTypedText.length + "</p>" +
                 "<p>Words Typed: " + typedWords + "</p>" +
                 "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
                 "<p>Words Per Minute (WPM): " + wpm + "</p>";
